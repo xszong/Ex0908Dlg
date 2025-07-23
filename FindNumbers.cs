fc384dfdd46745ca5731e7efe2f9ecb5d305616a
Xszong <987844869@qq.com>
2025-07-23 13:52:07 +0800

    添加项目文件。

---
+++ FindNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex0908Dlg
+{
+   class FindNumbers
+   {
+      //找出偶數
+      public void IsEven(params Int32[] numerical)
+      {
+         Write("偶數值：");
+         for (int k = 0; k < numerical.Length; k++)
+         {
+            //餘數為0
+            if (numerical[k] % 2 == 0)
+               Write($"{numerical[k],4}");
+         }
+         WriteLine();   //換行
+      }
+
+      //找出奇數
+      public void IsOdd(params Int32[] numerical)
+      {
+         Write("奇數值：");
+         for (int k = 0; k < numerical.Length; k++)
+         {
+            if (numerical[k] % 2 == 1)
+               Write($"{numerical[k],4}");
+         }
+         WriteLine();
+      }
+
+      //被3整除的數值
+      public void IsDivide3(params Int32[] numerical)
+      {
+         Write("被3整除的數值：");
+         for (int k = 0; k < numerical.Length; k++)
+         {
+            //被3整除
+            if (numerical[k] % 3 == 0)
+               Write($"{numerical[k],4}");
+         }
+         WriteLine();
+      }
+   }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ex0908Dlg
+{
+   class Program
+   {
+      //1.定義委派，含有一個引數(數值)
+      public delegate void Speculation(Int32[] numerical);
+
+      static void Main()   //主程式
+      {
+         //建立一個陣列
+         Int32[] figures =
+            {21, 32, 33, 142, 115, 125, 317, 188, 192, 420};
+
+         //建立物件
+         FindNumbers searchNum = new FindNumbers();
+
+         //2.FindNumbers類別所列示的方法成員
+         //3.宣告委派物件-單一任務
+         Speculation evenPredicate = new Speculation(searchNum.IsEven);
+
+         //3.1 委派多重任務
+         evenPredicate += searchNum.IsOdd;
+         evenPredicate += searchNum.IsDivide3;
+
+         //4.呼叫委派方法並以陣列為參數
+         evenPredicate(figures);
+
+         ReadKey();
+      }
+   }
+}
